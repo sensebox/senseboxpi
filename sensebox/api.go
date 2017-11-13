9d6d67eb20278234e0159b3c262894e5b7764d1d
ubergesundheit <ubergesundheit@googlemail.com>
2017-11-13 16:48:50 +0100

    experiment with api

--- sensebox/api.go
+++ sensebox/api.go
@@ -1,25 +1,49 @@
 package sensebox
 
 import (
+	"encoding/hex"
+	"errors"
 	"fmt"
 
 	"github.com/parnurzeal/gorequest"
 )
 
+const (
+	baseURL = "https://api.osem.vo1d.space/boxes"
+)
+
 type APIConnection struct {
-	baseUrl string
+	boxID string
 }
 
-type
+func validateID(id string) (err error) {
+	hex, err := hex.DecodeString(id)
+	if err != nil {
+		return
+	}
 
-func NewAPIConnection(baseUrl string) *APIConnection {
-	return &APIConnection{baseUrl: baseUrl}
+	if len(hex) != 12 {
+		return errors.New("id must be exactly 24 characters long")
+	}
+
+	return
+}
+
+func NewAPIConnection(boxID string) (*APIConnection, error) {
+	err := validateID(boxID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &APIConnection{boxID: boxID}, nil
 }
 
 func (a *APIConnection) FetchBox() {
-	_, body, errs := gorequest.New().Get("https://api.osem.vo1d.space/boxes/57fb712811347b0011c10e80").End()
+	var box SenseBox
+	_, _, errs := gorequest.New().Get(baseURL + "/" + a.boxID).EndStruct(&box)
 	if errs != nil {
 		fmt.Println(errs)
 	}
-	fmt.Println(body)
+
+	fmt.Println(box)
 }
--- sensebox/sensebox.go
+++ sensebox/sensebox.go
@@ -1,11 +1,22 @@
 package sensebox
 
+import "github.com/sensebox/senseboxpi/sensors"
+
 type SenseBoxSensor struct {
 	SensorType string `json:"sensorType"`
 	ID         string `json:"_id"`
 }
 
 type SenseBox struct {
-	ID      string
+	ID      string           `json:"_id"`
 	Sensors []SenseBoxSensor `json:"sensors"`
+}
+
+type SenseBoxSensorDevice struct {
+	Sensor       SenseBoxSensor
+	SensorDevice sensors.SensorDevice
+}
+
+func NewSenseBoxSensorDevice(senseBoxSensor SenseBoxSensor, sensorDevice sensors.SensorDevice) SenseBoxSensorDevice {
+	return SenseBoxSensorDevice{Sensor: senseBoxSensor, SensorDevice: sensorDevice}
 }
