99df85cb0278351abcad3fd5a54744fae050bdd1
ubergesundheit <ubergesundheit@googlemail.com>
2017-11-15 15:07:18 +0100

    refactor cmd/senseboxpi

--- cmd/senseboxpi/senseboxpi.go
+++ cmd/senseboxpi/senseboxpi.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"time"
 
@@ -9,12 +8,68 @@ import (
 	"github.com/sensebox/senseboxpi/sensors"
 )
 
-var (
-	hdc1008, tsl45315, veml6070, bmp280                       sensors.SensorDevice
-	tempSensor, humiSensor, lightSensor, uvSensor, presSensor sensebox.SenseBoxSensor
-)
+func initSenseBox() (sensebox.SenseBox, error) {
+	tempSensor := sensebox.Sensor{ID: ""}
+	humiSensor := sensebox.Sensor{ID: ""}
+	lightSensor := sensebox.Sensor{ID: ""}
+	uvSensor := sensebox.Sensor{ID: ""}
+	presSensor := sensebox.Sensor{ID: ""}
 
-func initSensors() (err error) {
+	box, err := sensebox.NewSenseBox("", &tempSensor, &humiSensor, &lightSensor, &uvSensor, &presSensor)
+	if err != nil {
+		return sensebox.SenseBox{}, err
+	}
+	return box, nil
+}
+
+func main() {
+	var (
+		tempSensor, humiSensor, lightSensor, uvSensor, presSensor sensebox.Sensor
+	)
+
+	hdc1008, tsl45315, veml6070, bmp280, err := initSensors()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	senseBox, err := initSenseBox()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	temperature, humidity, err := hdc1008.HDC100xTemperatureHumidity()
+	if err != nil {
+		log.Fatal(err)
+	}
+	tempSensor.AddMeasurement(temperature, time.Now())
+	humiSensor.AddMeasurement(humidity, time.Now())
+
+	lux, err := tsl45315.TSL4531Lux()
+	if err != nil {
+		log.Fatal(err)
+	}
+	lightSensor.AddMeasurement(lux, time.Now())
+
+	uv, err := veml6070.VEML6070UV()
+	if err != nil {
+		log.Fatal(err)
+	}
+	uvSensor.AddMeasurement(uv, time.Now())
+
+	pressure, err := bmp280.BMP280Pressure()
+	if err != nil {
+		log.Fatal(err)
+	}
+	presSensor.AddMeasurement(pressure, time.Now())
+
+	errs := senseBox.SubmitMeasurements()
+	if errs != nil {
+		log.Fatal(errs)
+	}
+
+}
+
+func initSensors() (hdc1008, tsl45315, veml6070, bmp280 sensors.SensorDevice, err error) {
 	hdc1008, err = sensors.NewHDC100xSensor()
 	if err != nil {
 		return
@@ -31,71 +86,5 @@ func initSensors() (err error) {
 	if err != nil {
 		return
 	}
-	return nil
-}
-
-func readSensors() (temperature, humidity, lux, uv, pressure float64, err error) {
-	temperature, humidity, err = hdc1008.HDC100xTemperatureHumidity()
-	if err != nil {
-		return
-	}
-	tempSensor.AddMeasurement(temperature, time.Now())
-	humiSensor.AddMeasurement(humidity, time.Now())
-	lux, err = tsl45315.TSL4531Lux()
-	if err != nil {
-		return
-	}
-	lightSensor.AddMeasurement(lux, time.Now())
-	uv, err = veml6070.VEML6070UV()
-	if err != nil {
-		return
-	}
-	uvSensor.AddMeasurement(uv, time.Now())
-	pressure, err = bmp280.BMP280Pressure()
-	if err != nil {
-		return
-	}
-	presSensor.AddMeasurement(pressure, time.Now())
 	return
-}
-
-func initSenseBox() (sensebox.SenseBox, error) {
-	tempSensor = sensebox.SenseBoxSensor{ID: ""}
-	humiSensor = sensebox.SenseBoxSensor{ID: ""}
-	lightSensor = sensebox.SenseBoxSensor{ID: ""}
-	uvSensor = sensebox.SenseBoxSensor{ID: ""}
-	presSensor = sensebox.SenseBoxSensor{ID: ""}
-
-	box, err := sensebox.NewSenseBox("", &tempSensor, &humiSensor, &lightSensor, &uvSensor, &presSensor)
-	if err != nil {
-		return sensebox.SenseBox{}, err
-	}
-	return box, nil
-}
-
-func main() {
-	err := initSensors()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	senseBox, err := initSenseBox()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	temperature, humidity, lux, uv, pressure, err := readSensors()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(temperature, humidity)
-	fmt.Println(pressure)
-	fmt.Println(lux)
-	fmt.Println(uv)
-
-	errs := senseBox.SubmitMeasurements()
-	if errs != nil {
-		log.Fatal(errs)
-	}
-
 }
